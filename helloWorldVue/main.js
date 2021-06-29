6fac9662f7b75ae9e2a8b5cb04e3adc95cdb3bfb
anbreaker <antunez19@gmail.com>
2021-06-29 12:14:22 +0200

    feat: learn vue by bluuweb

--- helloWorldVue/main.js
+++ helloWorldVue/main.js
@@ -2,6 +2,27 @@ const app = Vue.createApp({
   data() {
     return {
       title: 'First Hello World VueJs!',
+      link: 'https://google.es',
+      quantity: 400,
+      status: false,
+      services: ['transfers', 'payments', 'drafts'],
+      activate: false,
     };
+  },
+
+  methods: {
+    addCredit() {
+      this.quantity = this.quantity + 100;
+    },
+
+    reduceCredit(value) {
+      if (this.quantity <= 0) {
+        this.activate = true;
+        alert('Credit 0!!');
+        return;
+      }
+
+      this.quantity = this.quantity - value;
+    },
   },
 });
