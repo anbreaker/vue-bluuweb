e182e182c299f4887588c2c71d9602c2ff43e28b
anbreaker <antunez19@gmail.com>
2021-06-30 12:42:29 +0200

    feat: learn vuex init

---
+++ vuex-01/src/views/Home.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="home">
+    <h1 :style="colorCount">{{ title }}: {{ count }}</h1>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex';
+
+export default {
+  name: 'Home',
+
+  components: {},
+
+  data() {
+    return {
+      title: 'Count Vuex',
+    };
+  },
+
+  computed: {
+    ...mapState(['count']),
+    colorCount() {
+      return this.count >= 100 ? { color: 'green' } : { color: 'red' };
+    },
+  },
+};
+</script>
