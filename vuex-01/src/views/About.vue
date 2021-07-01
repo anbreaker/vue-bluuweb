e432ce6a5473db5a08e7c46f1a5fd7f489b09495
anbreaker <antunez19@gmail.com>
2021-07-01 08:41:20 +0200

    feat: v-model example

--- vuex-01/src/views/About.vue
+++ vuex-01/src/views/About.vue
@@ -1,5 +1,19 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+
+    <h2>V-model Example!</h2>
+    <input type="text" v-model="text" placeholder="Text Here!" />
+    <h2>{{ text }}</h2>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      text: '',
+    };
+  },
+};
+</script>
