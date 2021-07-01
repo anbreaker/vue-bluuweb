6b7e9b353d306628ebacd6929c5c4869c224a4d1
anbreaker <antunez19@gmail.com>
2021-07-01 10:38:12 +0200

    feat: learn use v-model input and checkbox

---
+++ form-01/src/views/Home.vue
@@ -0,0 +1,50 @@
+<template>
+  <form>
+    <input
+      type="text"
+      class="form-control my-2"
+      placeholder="Enter Name"
+      v-model="task.name"
+    />
+    <div>
+      <div class="form-check form-check-inline">
+        <input
+          class="form-check-input"
+          type="checkbox"
+          id="checkbox-1"
+          v-model="task.categories"
+          value="Javascript"
+        />
+        <label class="form-check-label" for="checkbox-1">Javascript</label>
+      </div>
+      <div class="form-check form-check-inline">
+        <input
+          class="form-check-input"
+          type="checkbox"
+          id="checkbox-2"
+          v-model="task.categories"
+          value="NodeJs"
+        />
+        <label class="form-check-label" for="checkbox-2">NodeJs</label>
+      </div>
+    </div>
+  </form>
+
+  <hr />
+  <p>{{ task }}</p>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  components: {},
+  data() {
+    return {
+      task: {
+        name: '',
+        categories: [],
+      },
+    };
+  },
+};
+</script>
