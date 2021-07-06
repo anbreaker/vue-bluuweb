b23808ea3541643776a7a39e2599a83dd5ae354c
anbreaker <antunez19@gmail.com>
2021-07-06 20:27:31 +0200

    change on html addTask

--- crud-firebase-vuejs2/src/views/AddTask.vue
+++ crud-firebase-vuejs2/src/views/AddTask.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
-    <h1>Add Task</h1>
+    <div class="row my-5">
+      <div class="col mb-2">
+        <h1 class="float-left">Add Task</h1>
+        <h1 class="float-right" v-if="user">{{ user.email }}</h1>
+      </div>
+    </div>
 
     <form @submit.prevent="addTask(name)" class="">
       <div class="input-group mb-2 mr-sm-2">
@@ -10,13 +15,13 @@
         <input type="text" class="form-control" v-model="name" />
       </div>
 
-      <button type="submit" class="btn btn-primary mb-2">Add Task</button>
+      <button type="submit" class="btn btn-primary mt-2">Add Task</button>
     </form>
   </div>
 </template>
 
 <script>
-import { mapActions } from 'vuex';
+import { mapActions, mapState } from 'vuex';
 
 export default {
   name: 'Add',
@@ -29,6 +34,9 @@ export default {
 
   methods: {
     ...mapActions(['addTask']),
+  },
+  computed: {
+    ...mapState(['user']),
   },
 };
 </script>
