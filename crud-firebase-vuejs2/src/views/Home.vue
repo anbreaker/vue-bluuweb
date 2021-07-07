7ef6b2eb88430face913e49e4a3149efc0018b2c
anbreaker <antunez19@gmail.com>
2021-07-07 21:20:06 +0200

    change color bootwatch and table

--- crud-firebase-vuejs2/src/views/Home.vue
+++ crud-firebase-vuejs2/src/views/Home.vue
@@ -1,14 +1,14 @@
 <template>
-  <div class="home">
+  <div>
     <div class="row my-5">
       <div class="col mb-2">
-        <h1 class="float-left">Home</h1>
+        <h1 class="float-left">TODO VueJs 2</h1>
         <h1 class="float-right" v-if="user">{{ user.email }}</h1>
       </div>
     </div>
 
     <router-link to="/add">
-      <button class="btn btn-success btn-block">Add Task</button>
+      <button class="btn btn-info btn-block">Add Task</button>
     </router-link>
 
     <div v-if="loading" class="text-center ">
@@ -17,23 +17,23 @@
     </div>
 
     <form class="mt-4" @submit.prevent="searchTaskByText(searchTask)">
-      <h4>Serch Task!</h4>
+      <h4 class="mb-4">Serch Task!</h4>
       <input
         class="form-control"
         type="text"
-        placeholder="Search"
+        placeholder="Search by name Task"
         v-model="searchTask"
         v-on:keyup="searchTaskByText(searchTask)"
       />
     </form>
 
-    <ul class="list-group mt-5" v-if="!loading">
+    <!-- <ul class="list-group mt-5" v-if="!loading">
       <li
         class="list-group-item"
         v-for="(item, index) in searchTaskArrayFilter"
         :key="index"
       >
-        {{ item.name }} - {{ item.id }}
+        {{ item.id }} - {{ item.name }}
 
         <div class="float-right">
           <router-link :to="{ name: 'Edit', params: { id: item.id } }">
@@ -44,7 +44,36 @@
           </button>
         </div>
       </li>
-    </ul>
+    </ul> -->
+
+    <table class="mt-3 table">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Id</th>
+          <th scope="col">Task</th>
+          <th class="text-right">Tools</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        <tr v-for="(item, index) in searchTaskArrayFilter" :key="index">
+          <th>{{ index + 1 }}</th>
+          <td>{{ item.id }}</td>
+          <td scope="row">{{ item.name }}</td>
+          <td>
+            <div class="float-right">
+              <router-link :to="{ name: 'Edit', params: { id: item.id } }">
+                <button class="btn btn-warning btn-sm mr-2">Edit</button>
+              </router-link>
+              <button @click="deleteTask(item.id)" class="btn btn-danger btn-sm">
+                Delete
+              </button>
+            </div>
+          </td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
